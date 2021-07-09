042631f79a24bcade939b5ab3f610074b5151e9b
raynasun0109 <sunmi@tcd.ie>
2021-07-09 22:51:30 +0100

    eject

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import styles from './App.css';
+import React from 'react';
+import {Switch, Router, Route} from 'react-router-dom';
+import history from './history';
+import Home from "../src/pages/Home/index";
+import Detail from "../src/pages/Detail/index";
+import Header from "./components/Header/header";
+import Footer from "./components/Footer/footer";
+class App extends React.Component {
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <Router history={history}>
+          <Header/>
+          <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/detail" component={Detail} />
+          </Switch>
+          <Footer/>
+      </Router>
     </div>
   );
+  }
 }
-
 export default App;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+// import styles from "index.less";
+import "./index.less"
+class Index extends Component {
+    render() {
+        return (
+            <div className="container">
+               home
+            </div>
+        );
+    }
+}
+
+export default Index;
